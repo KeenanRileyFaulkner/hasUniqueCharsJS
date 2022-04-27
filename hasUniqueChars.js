407e357be6e2ea8fae8e423e748c13e852baa9bb
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-04-26 20:39:58 -0600

    Added support for other data types (convert to string type)

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -2,6 +2,7 @@
 
 function stringHasUniqueChars(string) {
     let mySet = new Set();
+    string = String(string);
     for(let char of string) {
         if(mySet.has(char)) {
             return false;
@@ -16,3 +17,5 @@ function stringHasUniqueChars(string) {
 console.log(stringHasUniqueChars("monday"));
 console.log(stringHasUniqueChars("moonday"));
 console.log(stringHasUniqueChars("MarshmelLow"));
+console.log(stringHasUniqueChars(123456));
+console.log(stringHasUniqueChars(32132246));
